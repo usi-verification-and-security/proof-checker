e4e06871626b421873896d6fb567716e0ded19cd
Antti Hyvärinen <antti.hyvarinen@gmail.com>
2022-06-10 16:58:46 +0200

    proofparser: add an option for extracting logic name from proof

--- proofparser.cc
+++ proofparser.cc
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include "base64.h"
 
 class ParseException : public std::exception {
     std::string why;
 public:
-    ParseException(std::string const & why) : why(why) {}
-    virtual const char * what() { return why.c_str(); }
+    explicit ParseException(std::string && why) : why(std::move(why)) {}
+    const char * what() const noexcept override { return why.c_str(); }
 };
 
-std::string_view parseProof(std::string const & proofString) {
+std::pair<std::string,unsigned long> getLogicFromProof(std::string const & proofString) {
     auto initPos = proofString.find("(proof ");
     if (initPos == std::string::npos) {
         throw ParseException("Error: proof not found");
@@ -25,7 +26,12 @@ std::string_view parseProof(std::string const & proofString) {
         throw ParseException("Error: unterminated logic string");
     }
 
-    std::cout << "Found a proof string for logic `" << proofString.substr(logicStart, logicEnd - logicStart) << '`' << std::endl;
+    return {proofString.substr(logicStart, logicEnd - logicStart), logicEnd};
+}
+
+std::string_view parseProof(std::string && proofString) {
+
+    auto [logic, logicEnd] = getLogicFromProof(proofString);
 
     auto proofStart = proofString.find_first_not_of(" \t", logicEnd);
 
@@ -47,14 +53,32 @@ std::string_view parseProof(std::string const & proofString) {
 }
 
 int main(int argc, char ** argv) {
-    if (argc == 1) {
-        std::cout << "Usage: " << argv[0] << " <proof-file>" << std::endl;
+    if (argc != 3 || std::string(argv[1]) == "-h") {
+        std::cerr << "Proofparser -- Extract base64-encoded compressed trail from the opensmt trail format\n\n";
+        std::cerr << "Usage: " << argv[0] << " [-t | -l] <trail-file>\n";
+        std::cerr << "     -t   - print the trail\n";
+        std::cerr << "     -l   - print only the logic and not the trail\n" << std::endl;
         return 1;
     }
-    std::ifstream t(argv[1]);
+    if (argv[1] != std::string("-t") and argv[1] != std::string("-l")) {
+        std::cerr << "Unknown option: " << argv[1] << std::endl;
+        return 1;
+    }
+    std::ifstream t(argv[2]);
     std::stringstream buffer;
     buffer << t.rdbuf();
-    std::string proofString = buffer.str();
-    auto proof = parseProof(proofString);
-    std::cout << proof << std::endl;
+    try {
+        if (std::string(argv[1]) == "-l") {
+            auto [logic, pos] = getLogicFromProof(buffer.str());
+            std::cout << logic << std::endl;
+        } else if (std::string(argv[1]) =="-t") {
+            std::string decodedProof = base64_decode(parseProof(buffer.str()));
+            std::cout << decodedProof;
+        } else {
+            assert(false);
+        }
+    } catch (ParseException & e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
 }
